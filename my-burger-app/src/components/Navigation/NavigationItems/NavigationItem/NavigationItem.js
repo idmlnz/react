bd16b87cbac3c1f9de9bc20aa77681a03048e557
Ismelito Malonzo <idmlnz@yahoo.com>
2020-11-02 12:53:05 -0800

    Added assets and fixed Navigation

--- my-burger-app/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ my-burger-app/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './NavigationItem.css';
 
 const navigationItem = (props) => (
-        <li classes={classes.NavigationItem}>
+        <li className={classes.NavigationItem}>
             <a 
                     href={props.link}
                     className={props.active ? classes.active : null}>{props.children}
