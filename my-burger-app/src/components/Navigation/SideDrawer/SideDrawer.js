ae62886e33ae8369974f6c585ea47fcb172c4f3a
Ismelito Malonzo <idmlnz@yahoo.com>
2020-11-06 14:57:38 -0800

    Refactored code

--- my-burger-app/src/components/Navigation/SideDrawer/SideDrawer.js
+++ my-burger-app/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,7 +4,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems  from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 
 const sideDrawer = (props) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
@@ -24,7 +24,7 @@ const sideDrawer = (props) => {
             </div>
         </Aux>
     ); 
-}
+};
 
 
 export default sideDrawer;
